3372efa35513b479a0aed68c7a835c2ba4898be0
Pitt Wu <kgb00128@gmail.com>
2023-11-25 20:40:11 +0800

    feat: init scoket

--- src/app.js
+++ src/app.js
@@ -2,13 +2,30 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io');
+const { disconnect } = require('process');
 
 const server = http.createServer(app);
 
 app.use(cors());
 
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
 app.get('/', (req, res) => {
   res.send('Server is started');
+});
+
+io.on('connection', (socket) => {
+  console.log(`user is connected : ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    disconnectEventHandler(socket.id);
+  });
 });
 
 const PORT = process.env.PORT || 3003;
@@ -16,3 +33,8 @@ const PORT = process.env.PORT || 3003;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// Socket events
+const disconnectEventHandler = (id) => {
+  console.log(`user is disconnected : ${id}`);
+};
